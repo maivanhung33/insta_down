b57c1af9987d3cce48107c1d213d9053c3b30b36
WestlifeNo2 <WestlifeNo2@users.noreply.github.com>
2020-04-29 07:53:00 +0700

    Add validate post and validate profile
    Need more optimize in the future, leave it to me. But first it ran, that's the main point lol

--- insta_down/module/validator.py
+++ insta_down/module/validator.py
@@ -1,5 +1,5 @@
 from insta_down.module.insta_api import InstaAPI
-
+import requests
 
 class Validator:
     __url: str
@@ -10,11 +10,22 @@ class Validator:
         self.__insta_api = InstaAPI()
 
     def validate_post(self):
-        pass
         # return shortcode if link is post link of instagram else Raise BadRequest Exception
-        return
+        short_code = self.__url.split("/")
+        if (short_code[3] == "p") and (len(short_code) == 6):
+            return short_code[4]
+        else: 
+            pass
 
     def validate_profile(self):
-        pass
         # return userId if link is profile of user else Raise BadRequest Exception
-        return
+        url = self.__url
+        if url[-1:] != '/':
+            url += '/'
+        url = url + "?__a=1"
+        try: 
+            data = requests.get(url).json()
+            user_id = data["logging_page_id"].replace("profilePage_", "")
+            return user_id
+        except:
+            pass
